a0d22ad5912f09b26f36f1318c8bacae87312871
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2013-01-30 01:11:45 +0530

    Removed dead code

--- filter.go
+++ filter.go
@@ -18,6 +18,11 @@ func filterLogs(logs []Log, predicate func(*Log) bool) []Log {
 	return oplogs
 }
 
+//TODO:
+//	- n number
+//  -since date
+//  -grep date
+
 //TODO: change all int64 to int
 func getFilter(n int64, days int) Filterer {
 	if days != 0 {
@@ -30,58 +35,3 @@ func getFilter(n int64, days int) Filterer {
 	}
 	return &LineFilter{Number: n}
 }
-
-//========================================
-//========================================
-
-//TODO:
-//	- n number
-//  -since date
-//  -grep date
-
-func readLatestLogs(n int64) []Log {
-	return make([]Log, 10)
-}
-
-////if report is none it means we are logging a task
-//switch {
-//case len(os.Args) == 1 || *lineCount != -1:
-//printLatestLogs(*lineCount)
-//case *days != -1:
-//printLogsForDays(*days)
-//case *report == "none":
-//logTask(strings.Join(os.Args[1:], " "))
-//default:
-//generateReport(*report)
-//}
-func getLogs() []Log {
-	return readLatestLogs(10)
-}
-
-func printSummary(logs []Log) {
-}
-
-func printLogsForDays(days int) {
-	tillDate := time.Now().AddDate(0, 0, -1*(days-1))
-	tillDate = roundOffToDate(tillDate)
-	logs := readLatestLogs(int64(days) * MAX_LOGS_PER_DAY)
-	logs = filterLogs(logs, func(l *Log) bool { return l.End.After(tillDate) })
-	printSummary(logs)
-}
-
-func printLatestLogs(n int64) {
-
-	//this is when no option is given 
-	if n == -1 {
-		n = 10
-	}
-	logs := readLatestLogs(n)
-	lindex := int64(len(logs)) - n
-	//if we have less lines than the lineCount, show all the lines
-	if lindex < 0 {
-		lindex = 0
-	}
-	printSummary(logs[lindex:])
-}
-
-//utility functions
